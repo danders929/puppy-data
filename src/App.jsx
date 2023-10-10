b6a8be65691ed3c6ff75e1a5671d4cfc11b2f4e9
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-10-10 16:04:21 -0400

    finished mapping over the data

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
-  console.log(puppyList);
-
-  return <></>;
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList);
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
