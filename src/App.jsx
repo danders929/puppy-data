f3627210644bc0d4bc8a8236e4221f3893a49f03
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-10-10 15:41:41 -0400

    adding puppyList

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
+  console.log(puppyList);
+
   return <></>;
 }
 
