a0fc46697da985b2df352b4eb1fa40bd97b1f9eb
Dan <sandersda@gmail.com>
2023-10-10 15:53:18 -0500

    added Featured Pup

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,25 @@ import { useState } from "react";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
   console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <div className="App">
+      {featPupId && (
+        <div className ="feature">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
-      })}
+        return <div onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</div>;
+      })}     
     </div>
   );
 }
-
 export default App;
